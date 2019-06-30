c330f0be26d78113e65dfcdc937c4fbfeaf87321
unknown <magesh.sivagnanam@gmail.com>
2019-06-30 23:44:27 +0800

    added the spark session

--- spark-streaming-java/src/main/java/com/streaming/utils/SparkUtils.java
+++ spark-streaming-java/src/main/java/com/streaming/utils/SparkUtils.java
@@ -1,5 +1,24 @@
 package com.streaming.utils;
 
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * This class has set of spark related  utility methods
+ * @author Magesh
+ * 30/06/2019
+ */
+
 public class SparkUtils {
 
+	private SparkSession sparkSession = null;
+	
+	
+	
+	public SparkSession getSparkSession() {
+		 if (sparkSession == null) {
+			  sparkSession =  SparkSession.builder().getOrCreate();
+		 }
+		 return sparkSession;
+	}
+	
 }
